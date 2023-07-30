dc5b1bddd3d5538f751a0b1b67029635a76330a5
nitrous ls <nitrous.1320@outlook.com>
2023-07-30 16:12:05 +0300

    30/7/2023

---
+++ Notes-App-Integration-v.01/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes_App_Integration_v._01.Model;
+
+namespace Notes_App_Integration_v._01.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Notes> Notes { get; set; }
+    }
+}
---
+++ Notes-App-Integration-v.01/Model/Notes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes_App_Integration_v._01.Model
+{
+    public class Notes
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public String title { get; set; }
+        [Required]
+        public string content { get; set; }
+
+    }
+}
